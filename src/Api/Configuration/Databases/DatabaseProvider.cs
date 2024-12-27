b9417f8bc0d1060eaeb789829c716b28c1b71ea8
Matt Dunlap <trinitrotoluene5@gmail.com>
2024-12-27 11:20:12 -0500

    Add doc comments

--- src/Api/Configuration/Databases/DatabaseProvider.cs
+++ src/Api/Configuration/Databases/DatabaseProvider.cs
@@ -1,6 +1,12 @@
 namespace Api.Configuration.Databases;
 
+/// <summary>
+/// Represents the database provider.
+/// </summary>
 public enum DatabaseProvider
 {
+    /// <summary>
+    /// The datbase provider is SQLite.
+    /// </summary>
     SQLite = 1
 }
