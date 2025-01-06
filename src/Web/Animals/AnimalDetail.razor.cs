bffe65b591530232474913f3c735fcd98472492c
Matt Dunlap <trinitrotoluene5@gmail.com>
2025-01-06 01:09:39 -0500

    Add delete animal button

--- src/Web/Animals/AnimalDetail.razor.cs
+++ src/Web/Animals/AnimalDetail.razor.cs
@@ -37,11 +37,52 @@ public sealed partial class AnimalDetail(IShelteredClient shelteredClient, Navig
     public AnimalModel? Animal { get; private set; }
 
     /// <summary>
+    /// Gets or sets a <see cref="bool"/> representing if there are errors when deleting.
+    /// </summary>
+    /// <value>true if deleting has errors; otherwise false.</value>
+    public bool HasDeletionErrors { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets a <see cref="string"/> representing the deletion error message.
+    /// </summary>
+    /// <value>The last erorr message from a failed deletion.</value>
+    public string DeletionErrorMessage { get; set; } = string.Empty;
+
+    /// <summary>
     /// Navigates to the update animal page.
     /// </summary>
     public void NavigateToUpdateAnimalPage()
     {
         navigationManager.NavigateTo($"animals/update/{Id}");
+    }
+
+    /// <summary>
+    /// Asynchronously deletes the animal. Navigating back to the dashboard, if successful.
+    /// </summary>
+    /// <returns>The <see cref="Task"/> representing the asynchronous operation.</returns>
+    public async Task DeleteAnimalAsync()
+    {
+        HasDeletionErrors = false;
+        DeletionErrorMessage = string.Empty;
+
+        try
+        {
+            var successful = await shelteredClient.DeleteAnimalByIdAsync(Id);
+            if (successful)
+            {
+                navigationManager.NavigateTo("animals");
+            }
+            else
+            {
+                HasDeletionErrors = true;
+                DeletionErrorMessage = "The animal could not be deleted as it does not exist.";
+            }
+        }
+        catch
+        {
+            HasDeletionErrors = true;
+            DeletionErrorMessage = "An unknown error occurred, please try again momentarily.";
+        }
     }
 
     /// <inheritdoc/>
