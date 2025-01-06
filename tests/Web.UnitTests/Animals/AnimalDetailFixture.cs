2c382cb5681622e1f9f817ffbf3e8c59d993fdaa
Matt Dunlap <trinitrotoluene5@gmail.com>
2025-01-06 14:41:43 -0500

    Test animal detail delete button

--- tests/Web.UnitTests/Animals/AnimalDetailFixture.cs
+++ tests/Web.UnitTests/Animals/AnimalDetailFixture.cs
@@ -255,7 +255,8 @@ internal sealed class AnimalDetailFixture
                         <label>Kind</label>
                         <span>Cat</span>
                     </div>
-                    <button class=""btn btn-primary"">Update Animal</button>
+                    <button id=""updateButton"" class=""btn btn-primary"">Update Animal</button>
+                    <button id=""deleteButton"" class=""btn btn-primary"">Delete Animal</button>
                 ");
             }, Throws.Nothing);
         });
@@ -302,6 +303,188 @@ internal sealed class AnimalDetailFixture
             var expectedRelativeUrl = $"animals/update/{id}";
             var expected = navigationManager.BaseUri + expectedRelativeUrl;
             Assert.That(navigationManager.Uri, Is.EqualTo(expected));
+        });
+    }
+
+    [Test]
+    public void Should_render_the_templated_html_When_clicking_the_delete_animal_button_And_the_sheltered_client_throws_an_exception()
+    {
+        using var testContext = new Bunit.TestContext();
+
+        var id = Guid.NewGuid();
+        var animalModel = new AnimalModel
+        {
+            Name = "Lucy",
+            Kind = AnimalKind.Cat
+        };
+        using var shelteredClient = Substitute.For<IShelteredClient>();
+        shelteredClient
+            .GetAnimalByIdAsync(Arg.Is(id), Arg.Is(CancellationToken.None))
+            .Returns(animalModel);
+
+        shelteredClient
+            .DeleteAnimalByIdAsync(Arg.Is(id), Arg.Is(CancellationToken.None))
+            .ThrowsAsync<HttpRequestException>();
+
+        testContext.Services.AddSingleton(shelteredClient);
+
+        testContext.ComponentFactories.AddStub<PageTitle>();
+
+        using var animalDetailPage = testContext.RenderComponent<AnimalDetail>(parameters =>
+        {
+            parameters.Add(parameter => parameter.Id, id);
+        });
+
+        var deleteButton = animalDetailPage.Find("#deleteButton");
+        deleteButton.Click();
+
+        using var pageTitleStub = animalDetailPage.FindComponent<Stub<PageTitle>>();
+        using var pageTitle = testContext.Render(pageTitleStub.Instance.Parameters.Get(title => title.ChildContent)!);
+
+        var navigationManager = testContext.Services.GetRequiredService<FakeNavigationManager>();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(async () =>
+            {
+                _ = await shelteredClient
+                    .Received(Quantity.Exactly(1))
+                    .GetAnimalByIdAsync(Arg.Is(id), Arg.Is(CancellationToken.None));
+            }, Throws.Nothing);
+            Assert.That(navigationManager.History, Is.Empty);
+            Assert.That(pageTitle.Markup, Is.EqualTo("Lucy"));
+            Assert.That(() =>
+            {
+                animalDetailPage.MarkupMatches(@"
+                    <h1>Lucy</h1>
+                    <div>
+                        <label>Name</label>
+                        <span>Lucy</span>
+                        <label>Kind</label>
+                        <span>Cat</span>
+                    </div>
+                    <button id=""updateButton"" class=""btn btn-primary"">Update Animal</button>
+                    <button id=""deleteButton"" class=""btn btn-primary"">Delete Animal</button>
+                    <div>An unknown error occurred, please try again momentarily.</div>
+                ");
+            }, Throws.Nothing);
+        });
+    }
+
+    [Test]
+    public void Should_render_the_templated_html_When_clicking_the_delete_animal_button_And_the_sheltered_client_cannot_find_the_animal()
+    {
+        using var testContext = new Bunit.TestContext();
+
+        var id = Guid.NewGuid();
+        var animalModel = new AnimalModel
+        {
+            Name = "Lucy",
+            Kind = AnimalKind.Cat
+        };
+        using var shelteredClient = Substitute.For<IShelteredClient>();
+        shelteredClient
+            .GetAnimalByIdAsync(Arg.Is(id), Arg.Is(CancellationToken.None))
+            .Returns(animalModel);
+
+        shelteredClient
+            .DeleteAnimalByIdAsync(Arg.Is(id), Arg.Is(CancellationToken.None))
+            .Returns(false);
+
+        testContext.Services.AddSingleton(shelteredClient);
+
+        testContext.ComponentFactories.AddStub<PageTitle>();
+
+        using var animalDetailPage = testContext.RenderComponent<AnimalDetail>(parameters =>
+        {
+            parameters.Add(parameter => parameter.Id, id);
+        });
+
+        var deleteButton = animalDetailPage.Find("#deleteButton");
+        deleteButton.Click();
+
+        using var pageTitleStub = animalDetailPage.FindComponent<Stub<PageTitle>>();
+        using var pageTitle = testContext.Render(pageTitleStub.Instance.Parameters.Get(title => title.ChildContent)!);
+
+        var navigationManager = testContext.Services.GetRequiredService<FakeNavigationManager>();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(async () =>
+            {
+                _ = await shelteredClient
+                    .Received(Quantity.Exactly(1))
+                    .GetAnimalByIdAsync(Arg.Is(id), Arg.Is(CancellationToken.None));
+            }, Throws.Nothing);
+            Assert.That(navigationManager.History, Is.Empty);
+            Assert.That(pageTitle.Markup, Is.EqualTo("Lucy"));
+            Assert.That(() =>
+            {
+                animalDetailPage.MarkupMatches(@"
+                    <h1>Lucy</h1>
+                    <div>
+                        <label>Name</label>
+                        <span>Lucy</span>
+                        <label>Kind</label>
+                        <span>Cat</span>
+                    </div>
+                    <button id=""updateButton"" class=""btn btn-primary"">Update Animal</button>
+                    <button id=""deleteButton"" class=""btn btn-primary"">Delete Animal</button>
+                    <div>The animal could not be deleted as it does not exist.</div>
+                ");
+            }, Throws.Nothing);
+        });
+    }
+
+    [Test]
+    public void Should_navigate_to_the_animals_dashboard_When_clicking_the_delete_animal_button_And_the_sheltered_client_successfully_deletes_the_animal()
+    {
+        using var testContext = new Bunit.TestContext();
+
+        var id = Guid.NewGuid();
+        var animalModel = new AnimalModel
+        {
+            Name = "Lucy",
+            Kind = AnimalKind.Cat
+        };
+        using var shelteredClient = Substitute.For<IShelteredClient>();
+        shelteredClient
+            .GetAnimalByIdAsync(Arg.Is(id), Arg.Is(CancellationToken.None))
+            .Returns(animalModel);
+
+        shelteredClient
+            .DeleteAnimalByIdAsync(Arg.Is(id), Arg.Is(CancellationToken.None))
+            .Returns(true);
+
+        testContext.Services.AddSingleton(shelteredClient);
+
+        testContext.ComponentFactories.AddStub<PageTitle>();
+
+        using var animalDetailPage = testContext.RenderComponent<AnimalDetail>(parameters =>
+        {
+            parameters.Add(parameter => parameter.Id, id);
+        });
+
+        var deleteButton = animalDetailPage.Find("#deleteButton");
+        deleteButton.Click();
+
+        using var pageTitleStub = animalDetailPage.FindComponent<Stub<PageTitle>>();
+        using var pageTitle = testContext.Render(pageTitleStub.Instance.Parameters.Get(title => title.ChildContent)!);
+
+        var navigationManager = testContext.Services.GetRequiredService<FakeNavigationManager>();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(async () =>
+            {
+                _ = await shelteredClient
+                    .Received(Quantity.Exactly(1))
+                    .GetAnimalByIdAsync(Arg.Is(id), Arg.Is(CancellationToken.None));
+            }, Throws.Nothing);
+            const string expectedRelativeUrl = "animals";
+            var expected = navigationManager.BaseUri + expectedRelativeUrl;
+            Assert.That(navigationManager.Uri, Is.EqualTo(expected));
+            Assert.That(pageTitle.Markup, Is.EqualTo("Lucy"));
         });
     }
 }
