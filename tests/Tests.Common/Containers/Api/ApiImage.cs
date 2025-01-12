219087b0de78d6ecd106cb5bc63371a25228ed0b
Matt Dunlap <trinitrotoluene5@gmail.com>
2025-01-12 18:22:25 -0500

    Add documentation

--- tests/Tests.Common/Containers/Api/ApiImage.cs
+++ tests/Tests.Common/Containers/Api/ApiImage.cs
@@ -7,14 +7,28 @@ using DotNet.Testcontainers.Images;
 
 namespace Tests.Common.Containers.Api;
 
+/// <summary>
+/// A test container image for the api.
+/// </summary>
 public sealed class ApiImage : IImage
 {
+    /// <summary>
+    /// The http port to which the api will internally bind.
+    /// </summary>
     public const ushort HttpPort = 80;
 
     private readonly SemaphoreSlim _semaphoreSlim = new(1, 1);
 
     private readonly DockerImage _image = new("localhost/testcontainers", "api", DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString());
 
+    /// <summary>
+    /// Asynchronously builds the image.
+    /// </summary>
+    /// <remarks>
+    /// Calls to build the api image will lock the resource until the build completes, as a result the same image cannot
+    /// be built multiple times concurrently.
+    /// </remarks>
+    /// <returns>The <see cref="Task"/> representing the asynchronous operation.</returns>
     public async Task StartAsync()
     {
         await _semaphoreSlim.WaitAsync().ConfigureAwait(false);
@@ -37,24 +51,63 @@ public sealed class ApiImage : IImage
         }
     }
 
+    /// <inheritdoc/>
     public async Task DisposeAsync()
     {
         await Task.CompletedTask;
     }
 
+    /// <summary>
+    /// Gets the repository.
+    /// </summary>
+    /// <value>The repository</value>
     public string Repository => _image.Repository;
 
+    /// <summary>
+    /// Gets the tag.
+    /// </summary>
+    /// <value>The tag.</value>
     public string Tag => _image.Tag;
 
+    /// <summary>
+    /// Gets the full image name.
+    /// </summary>
+    /// <value>The full image name.</value>
     public string FullName => _image.FullName;
 
+    /// <summary>
+    /// Throws a <see cref="NotImplementedException"/>.
+    /// </summary>
+    /// <exception cref="NotImplementedException"/>
     public string Registry => throw new NotImplementedException();
 
+    /// <summary>
+    /// Throws a <see cref="NotImplementedException"/>.
+    /// </summary>
+    /// <exception cref="NotImplementedException"/>
     public string Digest => throw new NotImplementedException();
 
+    /// <summary>
+    /// Gets the registry hostname.
+    /// </summary>
+    /// <returns>The registry hostname.</returns>
     public string GetHostname() => _image.GetHostname();
 
+    /// <summary>
+    /// Throws a <see cref="NotImplementedException"/>.
+    /// </summary>
+    /// <exception cref="NotImplementedException"/>
     public bool MatchLatestOrNightly() => throw new NotImplementedException();
+
+    /// <summary>
+    /// Throws a <see cref="NotImplementedException"/>.
+    /// </summary>
+    /// <exception cref="NotImplementedException"/>
     public bool MatchVersion(Predicate<string> predicate) => throw new NotImplementedException();
+
+    /// <summary>
+    /// Throws a <see cref="NotImplementedException"/>.
+    /// </summary>
+    /// <exception cref="NotImplementedException"/>
     public bool MatchVersion(Predicate<Version> predicate) => throw new NotImplementedException();
 }
