6cf8456b37225f9692d3cf38d844f3ec2d325226
Matt Dunlap <trinitrotoluene5@gmail.com>
2025-01-16 04:57:17 -0500

    Update AnimalImageMapper.Create to convert uri location to a string and update docs

--- src/Api/Animals/AnimalImageMapper.cs
+++ src/Api/Animals/AnimalImageMapper.cs
@@ -10,14 +10,14 @@ namespace Api.Animals;
 public interface IAnimalImageMapper
 {
     /// <summary>
-    /// Creates a new <see cref="AnimalImageEntity"/> from an <see cref="AnimalImageModel"/>.
+    /// Creates a new <see cref="AnimalImageEntity"/> from an <paramref name="animalId"/> and <paramref name="location"/>.
     /// </summary>
     /// <param name="animalId">
     /// A <see cref="Guid"/> representing the id of the <see cref="AnimalEntity"/> to which the image belongs.
     /// </param>
-    /// <param name="location">The location of the image.</param>
+    /// <param name="location">A <see cref="Uri"/> representing the location of the image.</param>
     /// <returns>A new <see cref="AnimalImageEntity"/>.</returns>
-    AnimalImageEntity Create(Guid animalId, string location);
+    AnimalImageEntity Create(Guid animalId, Uri location);
 
     /// <summary>
     /// Creates a new <see cref="AnimalModel"/> from an <see cref="AnimalEntity"/>.
@@ -30,11 +30,11 @@ public interface IAnimalImageMapper
 /// <inheritdoc cref="IAnimalImageMapper"/>
 public sealed class AnimalImageMapper : IAnimalImageMapper
 {
-    /// <inheritdoc cref="IAnimalImageMapper.Create(Guid, string)"/>
-    public AnimalImageEntity Create(Guid animalId, string location) => new()
+    /// <inheritdoc cref="IAnimalImageMapper.Create(Guid, Uri)"/>
+    public AnimalImageEntity Create(Guid animalId, Uri location) => new()
     {
         AnimalId = animalId,
-        Location = location
+        Location = location.ToString()
     };
 
     /// <inheritdoc cref="IAnimalImageMapper.Map(AnimalImageEntity)"/>
