cbfb5305b5da1de1c8558063ac0fbd11bb0d8874
Matt Dunlap <trinitrotoluene5@gmail.com>
2025-01-21 23:56:31 -0500

    Add the first image of the animal to the preview

--- src/Web/Animals/AnimalPreview.razor.cs
+++ src/Web/Animals/AnimalPreview.razor.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Components;
 using Client.Animals;
 
@@ -15,4 +16,26 @@ public sealed partial class AnimalPreview
     [Parameter]
     [EditorRequired]
     public required AnimalModel Animal { get; init; }
+
+    /// <summary>
+    /// Gets or inits the height of the image.
+    /// </summary>
+    /// <value>The height of the image in pixels.</value>
+    [Parameter]
+    [EditorRequired]
+    public required uint Height { get; init; }
+
+    /// <summary>
+    /// Gets or inits the width of the image.
+    /// </summary>
+    /// <value>The width of the image in pixels.</value>
+    [Parameter]
+    [EditorRequired]
+    public required uint Width { get; init; }
+
+    /// <summary>
+    /// Gets the location of the first image of the animal.
+    /// </summary>
+    /// <value>The location of the first image of the animal.</value>
+    private string Location => Animal.Images.First().Location;
 }
